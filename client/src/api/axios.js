a5767232ba01a770d1ff58e08ad9bf9d23b2ccb8
Deki <dejan.kelecevic@gmail.com>
2025-07-03 23:32:06 +0200

    final final

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,11 +2,13 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://88.200.63.148:4174/api',
-  // baseURL: 'http://localhost:4174/api',  Base URL for local development
+  baseURL: import.meta.env.DEV
+    ? 'http://localhost:4174/api' // local dev
+    : '/api', // production on Netlify
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export default instance;
+
